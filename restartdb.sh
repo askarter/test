a56ba55943f42f90fe209e448810fbfd5c043a21
Alexander Kolesnikov <alex.support.1@gmail.com>
2015-07-23 14:44:15 +0300

    Update restartdb.sh

--- restartdb.sh
+++ restartdb.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+exec >/dev/null
+exec 2>/dev/null
+(
+trap "" TERM
+while true
+do
 if [ -e /run/mysqld/mysqld.pid ]
 then
-exit
+echo "1" > /dev/null
 else
 systemctl restart mysqld > /dev/null && date >> /root/checkdb/restartdb.log && echo "restart mysqld" >> /root/checkdb/restartdb.log
 fi
 if [ -e /run/mysqld/mysqld.pid ]
 then
-exit
+echo "1" > /dev/null
 else
 date >> /root/checkdb/restartdb.log && echo "ERROR: restart dont successful" >> /root/checkdb/restartdb.log
 fi
-exit 0
+sleep 1
+done
+)&
